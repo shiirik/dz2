26d82d7cebd1cff3369ee84ead196e0698ad8846
shiirik <mr.pakman04@gmail.com>
2023-10-26 09:24:25 +0600

    Добавьте файлы проекта.

---
+++ dz2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите несколько чисел через пробел:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+        double sum = 0;
+
+        foreach (string number in numbers)
+        {
+            if (double.TryParse(number, out double parsedNumber))
+            {
+                sum += parsedNumber;
+            }
+            else
+            {
+                Console.WriteLine($"Неверное значение: {number}");
+            }
+        }
+
+        Console.WriteLine($"Сумма введенных чисел: {sum}");
+    }
+}
